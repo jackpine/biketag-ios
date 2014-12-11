d3e31eeecb02134206423a81e2c06fe7556fb821
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2014-12-11 11:19:23 -0800

    initial commit.
    Fresh swift app, started working on homescreen xib.

---
+++ BikeTag/BikeTag/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BikeTag
+//
+//  Created by Michael Kirk on 12/11/14.
+//  Copyright (c) 2014 Jackpine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
