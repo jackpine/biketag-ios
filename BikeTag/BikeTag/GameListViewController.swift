0a055850fe9b4180f099c6de6aa1fd7fd8904171
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2014-12-11 17:22:51 -0800

    implementing layout of homescreen w/ example photo

--- BikeTag/BikeTag/GameListViewController.swift
+++ BikeTag/BikeTag/GameListViewController.swift
@@ -1,14 +1,6 @@
-//
-//  FirstViewController.swift
-//  BikeTag
-//
-//  Created by Michael Kirk on 12/11/14.
-//  Copyright (c) 2014 Jackpine. All rights reserved.
-//
-
 import UIKit
 
-class FirstViewController: UIViewController {
+class GameListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
