4595715168d4efe588c9d79d43c7cd205ebec7bf
Michael Kirk <michael.john.kirk@gmail.com>
2015-03-12 16:44:57 -0700

    move Spot fetch to async operation out of HomeController

--- src/BikeTag/Models/Spot.swift
+++ src/BikeTag/Models/Spot.swift
@@ -8,4 +8,16 @@ class Spot: NSObject {
     self.isCurrentUserOwner = isCurrentUser
     self.image = image
   }
+
+  class func fetchCurrentSpot(callback:(Spot)->()) -> () {
+    dispatch_async(dispatch_get_main_queue(), {
+      //simulate network delay
+      sleep(1)
+      let initialImage = UIImage( named: "952 lucile" )!
+      let currentSpot = Spot(image: initialImage, isCurrentUser: false)
+
+      callback(currentSpot)
+    })
+  }
+
 }
