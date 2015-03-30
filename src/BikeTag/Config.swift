2f8c1a2ccea2e05b309bf5731539fba64bb91ab2
Michael Kirk <michael.john.kirk@gmail.com>
2015-03-30 15:53:41 -0700

    API endpoint dependins on Build Configuration

--- src/BikeTag/Config.swift
+++ src/BikeTag/Config.swift
@@ -5,21 +5,19 @@ private let sharedInstance = Config.Instance()
 
 class Config {
   class Instance {
-    var testing: Bool
-    var apiEndpoint: String
+    let testing: Bool
+    let apiEndpoint: String
 
     init() {
       self.testing = false
-      if ( true ) {
-        self.apiEndpoint = "http://api.biketag-staging.jackpine.me/api/v1/"
-      } else { //development
 
-        if ( UIDevice.currentDevice().model == "iPhone Simulator" ) {
-          self.apiEndpoint = "http://192.168.59.103:3000/api/v1/"
-        } else {
-          self.apiEndpoint = "http://10.0.1.53:3000/api/v1/"
-        }
-      }
+      let settingsPath = NSBundle.mainBundle().pathForResource("Settings", ofType: "plist")!
+      let settingsFromFile = NSDictionary(contentsOfFile: settingsPath)!
+      println("Loaded Config:")
+      println(settingsFromFile)
+
+      self.apiEndpoint = settingsFromFile["apiEndpoint"] as String
+
     }
   }
 
