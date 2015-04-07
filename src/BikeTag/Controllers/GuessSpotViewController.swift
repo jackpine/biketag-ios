8ae18d0951c03f929d564ca84dd18e2be95d0d91
Michael Kirk <michael.john.kirk@gmail.com>
2015-04-07 00:57:50 -0700

    move photo capture out of segue

--- src/BikeTag/Controllers/GuessSpotViewController.swift
+++ src/BikeTag/Controllers/GuessSpotViewController.swift
@@ -5,24 +5,28 @@ import CoreLocation
 class GuessSpotViewController: CameraViewController {
 
   var currentSpot: Spot?
+  var newGuess: Guess?
+
+  func createGuessFromData(imageData: NSData, location: CLLocation) -> () {
+    var image: UIImage?
+    if UIDevice.currentDevice().model == "iPhone Simulator" {
+      image = UIImage(named: "952 lucile")!
+    } else {
+      image = UIImage(data: imageData)!
+    }
+
+    self.newGuess = Guess(spot: self.currentSpot!, user: User.getCurrentUser(), location: location, image:image!)
+    self.performSegueWithIdentifier("showCheckingGuessSegue", sender: nil)
+  }
+
+  @IBAction func takePictureButtonViewTouched(sender: AnyObject) {
+    self.captureImage(createGuessFromData)
+  }
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) -> Void {
     super.prepareForSegue(segue, sender: sender)
-
-    let createImageFromData = {(imageData: NSData, location: CLLocation) -> () in
-      var image: UIImage?
-      if UIDevice.currentDevice().model == "iPhone Simulator" {
-        image = UIImage(named: "952 lucile")!
-      } else {
-        image = UIImage(data: imageData)!
-      }
-
-      let checkGuessViewController = segue.destinationViewController as CheckGuessViewController
-      let guess = Guess(spot: self.currentSpot!, user: User.getCurrentUser(), location: location, image:image!)
-      checkGuessViewController.guess = guess
-    }
-
-    self.captureImage(createImageFromData)
+    let checkGuessViewController = segue.destinationViewController as CheckGuessViewController
+    checkGuessViewController.guess = self.newGuess!
   }
 
 }
