6c9dd42f9c7da2f32ab7d75f69ad673265b302f4
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2015-06-01 09:32:27 -0700

    comment ApiService

--- src/BikeTag/Services/ApiService.swift
+++ src/BikeTag/Services/ApiService.swift
@@ -4,10 +4,9 @@ import Alamofire
 class ApiService {
   let apiEndpoint = NSURL(string: Config.apiEndpoint())!
 
-
   func request(request: NSURLRequest, handleResponseAttributes: (NSDictionary) -> (), errorCallback: (NSError)->() ) {
-    Alamofire.request(request)
-      .responseJSON { (request, response, json, error) in
+    Alamofire.request(request).responseJSON { (request, response, json, error) in
+        // Protocol level errors, e.g. connection timed out
         if( error != nil ) {
           Logger.warning("HTTP Error: \(error)")
           return errorCallback(error!)
@@ -15,6 +14,7 @@ class ApiService {
 
         let responseAttributes = json as! NSDictionary
 
+        // Application level errors e.g. missing required attribute
         if let apiError = responseAttributes["error"] as! [NSObject: AnyObject]? {
           Logger.error("API Error: \(apiError)")
           return errorCallback(APIError(errorDict: apiError))
