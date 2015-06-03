81cfc1e786681bf07f209b85e94bc1e3e58c96a1
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2015-06-03 09:27:04 -0700

    explicit "image-not-found" error page

--- src/BikeTag/Models/Spot.swift
+++ src/BikeTag/Models/Spot.swift
@@ -22,8 +22,7 @@ class Spot: NSObject {
   init(parsedSpot: ParsedSpot) {
     let imageData = NSData(contentsOfURL: parsedSpot.imageUrl)
     if( imageData == nil ) {
-      //TODO this is a super confusing error condition since this image is used all over the place in testing.
-      self.image = UIImage(named: "952 lucile")!
+      self.image = UIImage(named: "image-not-found")!
     } else {
       self.image = UIImage(data: imageData!)!
     }
