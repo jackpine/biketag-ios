6daf551ea40928c99eed0a9fa6f32193324f96af
Michael Kirk <michael.john.kirk@gmail.com>
2015-07-13 17:52:43 -0400

    don't lose images when we scroll back and forth

--- src/BikeTag/Views/SpotView.swift
+++ src/BikeTag/Views/SpotView.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Alamofire
 
+private var imageCache = [Int:UIImage]()
 class SpotView: UIImageView {
 
   required init(frame: CGRect, spot: Spot) {
@@ -8,11 +9,16 @@ class SpotView: UIImageView {
     self.contentMode = .ScaleAspectFill
     self.clipsToBounds = true
     let spotId = spot.id!
-    self.image = UIImage(named: "sketchy bike")
-    Alamofire.request(.GET, spot.imageUrl!).response() {
-      (_, _, data, _) in
-      let image = UIImage(data: data! as! NSData)
-      self.image = image
+    if imageCache[spotId] == nil {
+      self.image = UIImage(named: "sketchy bike")
+      Alamofire.request(.GET, spot.imageUrl!).response() {
+        (_, _, data, _) in
+        let image = UIImage(data: data! as! NSData)
+        self.image = image
+        imageCache[spotId] = image
+      }
+    } else {
+      self.image = imageCache[spotId]
     }
   }
 
