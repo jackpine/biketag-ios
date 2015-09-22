f6df227f03d288e181ad9b03fcd9665ecb6ee4b3
Michael Kirk <michael.john.kirk@gmail.com>
2015-09-22 12:51:02 -0500

    fleshing out a score UI

--- src/BikeTag/ApplicationViewController.swift
+++ src/BikeTag/ApplicationViewController.swift
@@ -4,5 +4,23 @@ import UIKit
 class ApplicationViewController: UIViewController {
 
   let spotsService = Config.fakeApiCalls() ? FakeSpotsService() : SpotsService()
+  var scoreButton: UIBarButtonItem?
+  let score = 100
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.scoreButton = UIBarButtonItem(title: "points", style: UIBarButtonItemStyle.Plain, target: self, action: "scoreButtonTouched")
+    self.scoreButton!.title = "● \(score)"
+    self.navigationItem.rightBarButtonItem = scoreButton
+  }
+
+  func scoreButtonTouched() {
+    Logger.debug("score button touched")
+  }
 
 }
