e4a76b61e8bb0b0549d0236d316a6d28aa8e1b5a
Michael Kirk <michael.john.kirk@gmail.com>
2016-02-26 12:06:49 -0800

    see new spot right away after adding

--- src/BikeTag/Controllers/NewSpotViewController.swift
+++ src/BikeTag/Controllers/NewSpotViewController.swift
@@ -43,6 +43,8 @@ class NewSpotViewController: CameraViewController {
     if self.newSpot != nil {
       homeViewController.updateGame(self.newSpot!.game, newSpot: self.newSpot!)
       homeViewController.currentSpot = self.newSpot!
+      homeViewController.currentSpots.insert(self.newSpot!, atIndex: 0)
+      homeViewController.gameTableView.contentOffset = CGPoint(x: 0, y: 0)
       homeViewController.refresh()
     }
   }
