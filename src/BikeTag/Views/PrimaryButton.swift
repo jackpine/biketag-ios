79ba0522f68ddecbcf315159f330e829f87b4fc9
Michael Kirk <michael.john.kirk@gmail.com>
2016-05-17 12:39:09 -0700

    remove border from primary button

--- src/BikeTag/Views/PrimaryButton.swift
+++ src/BikeTag/Views/PrimaryButton.swift
@@ -21,23 +21,9 @@ class PrimaryButton: UIButton {
 
   func applyStyle() {
     self.backgroundColor = UIColor.clearColor()
-    self.layer.cornerRadius = 8.0
-    self.layer.masksToBounds = true
-    self.layer.borderWidth = 1
     self.titleLabel?.font = Font.titleFont
     self.setTitleColor(Color.grayColor, forState: .Disabled)
     self.setTitleColor(Color.redColor, forState: .Normal)
-    applyStyle(state: self.state)
-  }
-
-  func applyStyle(state aState: UIControlState) {
-    switch(aState) {
-    case UIControlState.Disabled:
-      self.layer.borderColor = Color.grayColor.CGColor
-
-    default:
-      self.layer.borderColor = Color.redColor.CGColor
-    }
   }
   
 }
