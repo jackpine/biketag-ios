384299467c990e490589536d7e6d07e8318c3652
Michael Kirk <michael.john.kirk@gmail.com>
2016-05-17 21:54:23 -0700

    delegate methods at end of file

--- src/BikeTag/Services/LocationService.swift
+++ src/BikeTag/Services/LocationService.swift
@@ -8,25 +8,6 @@ class LocationService: NSObject, CLLocationManagerDelegate {
     super.init()
   }
 
-  func locationManager(manager: CLLocationManager,
-                       didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-
-    startTrackingLocation(onDenied: { Logger.error("sneaky user disabled location authorization status.") } )
-  }
-
-  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    guard (locations.last != nil) else {
-      Logger.error("location did update, but was nil")
-      return
-    }
-
-    if( self.mostRecentLocation == nil ) {
-      Logger.debug("Initialized location: \(locations.last)")
-    }
-    self.mostRecentLocation = locations.last
-  }
-
-  // MARK: CLLocationManagerDelegate
   func waitForLocation(onSuccess successCallback: (CLLocation)->(), onTimeout timeoutCallback: ()->()) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2.0 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
 
@@ -59,4 +40,22 @@ class LocationService: NSObject, CLLocationManagerDelegate {
     }
   }
 
+  // MARK: CLLocationManagerDelegate
+  func locationManager(manager: CLLocationManager,
+                       didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+
+    startTrackingLocation(onDenied: { Logger.error("sneaky user disabled location authorization status.") } )
+  }
+
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard (locations.last != nil) else {
+      Logger.error("location did update, but was nil")
+      return
+    }
+
+    if( self.mostRecentLocation == nil ) {
+      Logger.debug("Initialized location: \(locations.last)")
+    }
+    self.mostRecentLocation = locations.last
+  }
 }
